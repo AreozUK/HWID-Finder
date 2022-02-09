f27cf01189f10f2b9cf22f895136ecc014b551d9
areoz <73476457+AreozUK@users.noreply.github.com>
2022-02-09 21:50:20 +0000

    Add files via upload

---
+++ GrabHWID/Main.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HWID_Finder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GrabBTN = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GrabBTN
+            // 
+            this.GrabBTN.CheckedState.Parent = this.GrabBTN;
+            this.GrabBTN.CustomImages.Parent = this.GrabBTN;
+            this.GrabBTN.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.GrabBTN.Font = new System.Drawing.Font("Segoe UI Semibold", 9.25F, System.Drawing.FontStyle.Bold);
+            this.GrabBTN.ForeColor = System.Drawing.Color.White;
+            this.GrabBTN.HoverState.Parent = this.GrabBTN;
+            this.GrabBTN.Location = new System.Drawing.Point(12, 229);
+            this.GrabBTN.Name = "GrabBTN";
+            this.GrabBTN.ShadowDecoration.Parent = this.GrabBTN;
+            this.GrabBTN.Size = new System.Drawing.Size(338, 54);
+            this.GrabBTN.TabIndex = 0;
+            this.GrabBTN.Text = "Grab HWID";
+            this.GrabBTN.Click += new System.EventHandler(this.GrabBTN_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 211);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(20, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(319, 192);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(362, 295);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.GrabBTN);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button GrabBTN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ GrabHWID/Main.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HWID_Finder
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #region "Grab HWID Function"
+        private static string GrabHWID()
+        {
+            string Root = @"SOFTWARE\Microsoft\Cryptography";
+            string N = "MachineGuid";
+
+            using (RegistryKey localMachineX64View = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (RegistryKey RegKey = localMachineX64View.OpenSubKey(Root))
+                {
+                    if (RegKey == null)
+                    {
+                        throw new KeyNotFoundException(string.Format(Root));
+                    }
+                    object HWID = RegKey.GetValue(N);
+                    if (HWID == null)
+                    {
+                        throw new IndexOutOfRangeException(string.Format(N));
+                    }
+                    return HWID.ToString();
+                }
+            }
+        }
+        #endregion
+
+        private void GrabBTN_Click(object sender, EventArgs e)
+        {
+            GrabHWID();
+            richTextBox1.Text = "HWID: " + GrabHWID();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
